903738c49b2022a7abe4c539cedddb746074b4a8
Johan Andersson <johan-spam@andersson.net>
2020-10-12 16:04:14 +0200

    chore: add current repos to list

--- src/pypi-limeci.mjs
+++ src/pypi-limeci.mjs
@@ -1,7 +1,21 @@
-// Add a line for each repository that needs access using its name
+// Add a line for each repository that needs push access to pypi using its name
 // Example: 'limepkg-workorder',
 const repositories = [
-
+    'addon-infotiles',
+    'addon-lime-event',
+    'addon-lime-automation',
+    'lime-buildtools',
+    'lime-core',
+    'lime-crm',
+    'lime-dynaconf-consul-loader',
+    'lime-integration-tests',
+    'lime-newsletter',
+    'lime-project',
+    'limepkg-credit-info',
+    'limepkg-dynamic-checklist',
+    'limepkg-esign',
+    'limepkg-survey-integration',
+    'mtech-addon-shared',
 ]
 
 export default repositories;
